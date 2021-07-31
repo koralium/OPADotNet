30cb7a0598af3963e71237cb89302b61b8df4e0f
Alexander Östman <alexander.ostman@sweco.se>
2021-07-31 16:20:21 +0200

    Fixed so any calls are merged on iterator name.
    This fixes #10

--- netcore/src/OPADotNet.Expressions/Ast/Models/Reference.cs
+++ netcore/src/OPADotNet.Expressions/Ast/Models/Reference.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OPADotNet.Expressions.Ast.Models
@@ -11,6 +12,11 @@ namespace OPADotNet.Expressions.Ast.Models
         public override T Accept<T>(ExpressionAstVisitor<T> visitor)
         {
             return visitor.VisitReference(this);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(".", References.Select(x => x.Value));
         }
     }
 }
