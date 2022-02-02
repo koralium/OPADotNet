ff04c3c67463876da2164956f2a15b02ac07608c
Ulimo <alexander.ostman@hotmail.com>
2022-02-02 10:48:40 +0100

    Fixed bug that with property was not parsed from the AST. (#70)
    * Fixed bug that with property was not parsed from the AST.
    
    This fixes #67
    
    * Added more tests
    
    * Added test against other type for equals

---
+++ netcore/src/OPADotNet.Core/Ast/Models/AstWith.cs
@@ -0,0 +1,42 @@
+﻿using OPADotNet.Ast;
+using OPADotNet.Ast.Models;
+using OPADotNet.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace OPADotNet.Ast.Models
+{
+    public class AstWith : AstNode
+    {
+        [JsonPropertyName("target")]
+        public AstTerm Target { get; set; }
+
+        [JsonPropertyName("value")]
+        public AstTerm Value { get; set; }
+
+        public override T Accept<T>(AstVisitor<T> visitor)
+        {
+            return visitor.VisitWith(this);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is AstWith other)
+            {
+                return Equals(Target, other.Target) &&
+                    Equals(Value, other.Value);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hashCode = new HashCode();
+            hashCode.Add(Target);
+            hashCode.Add(Value);
+            return hashCode.ToHashCode();
+        }
+    }
+}
---
+++ netcore/tests/OPADotNet.Core.Tests/Ast/VisitorTests.cs
@@ -0,0 +1,56 @@
+﻿using Moq;
+using NUnit.Framework;
+using OPADotNet.Ast;
+using OPADotNet.Ast.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPADotNet.Core.Tests.Ast
+{
+    internal class VisitorTests
+    {
+        [Test]
+        public void TestVisitWith()
+        {
+            AstVisitor<object> visitor = new AstVisitor<object>();
+
+            Mock<AstTerm> target = new Mock<AstTerm>();
+            Mock<AstTerm> value = new Mock<AstTerm>();
+
+            AstWith with = new AstWith()
+            {
+                Target = target.Object,
+                Value = value.Object
+            };
+
+            visitor.Visit(with);
+
+            target.Verify(x => x.Accept(visitor), Times.Once);
+            value.Verify(x => x.Accept(visitor), Times.Once);
+        }
+
+        [Test]
+        public void TestVisitExpression()
+        {
+            AstVisitor<object> visitor = new AstVisitor<object>();
+
+            Mock<AstTerm> target1 = new Mock<AstTerm>();
+            Mock<AstWith> target2 = new Mock<AstWith>();
+
+            AstExpression expr = new AstExpression()
+            {
+                Index = 0,
+                Terms = new List<AstTerm>() { target1.Object },
+                With = new List<AstWith>() { target2.Object }
+            };
+
+            visitor.Visit(expr);
+
+            target1.Verify(x => x.Accept(visitor), Times.Once);
+            target2.Verify(x => x.Accept(visitor), Times.Once);
+        }
+    }
+}
